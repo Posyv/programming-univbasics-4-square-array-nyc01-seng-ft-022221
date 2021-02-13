d3c7b627bd3e05a24daaa8d7eeeae5bdf1253bf5
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 19:41:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  count = 0
+  squarednums = []
+  while count < array.length do
+    squarednums.push(array[count].to_s)
+    count = count**2
+  end
+  squarednums
 end

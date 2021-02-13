7f5814a3f9693e4db14e44491c774aa91847db3c
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 19:44:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  # count = 0
-  # squarednums = []
-  # while count < array.length do
-  #   squarednums.push(array[count].to_s)
-  #   count = count**2
-  # end
-  # squarednums
+  count = 0
+  squarednums = []
+  while count < array.length do
+    squarednums.push(array[count].to_s)
+    count = count**2
+  end
+  squarednums
 end
